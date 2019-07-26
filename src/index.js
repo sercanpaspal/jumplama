1b78088609ef6880c6833884d6b9def3e744c52f
Sercan PASPAL <seco@Sercan-MacBook-Air.local>
2019-07-26 18:22:20 +0300

    BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router,Route, Switch } from 'react-router-dom';
+import { HashRouter as Router,Route, Switch } from 'react-router-dom';
 import './index.css';
 
 import App from './components/App';
