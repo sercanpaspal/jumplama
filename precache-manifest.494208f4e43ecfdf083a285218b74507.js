8c108b50c7826d59aeac672437275344a097c9d4
Sercan PASPAL <seco@Sercan-MacBook-Air.local>
2019-07-26 18:16:39 +0300

    Updates

---
+++ precache-manifest.494208f4e43ecfdf083a285218b74507.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a662e8ab5334b698a97e6c3f3846fe",
+    "url": "/jumplama/index.html"
+  },
+  {
+    "revision": "b9413248ac205c7b72dc",
+    "url": "/jumplama/static/css/main.55cd0fb3.chunk.css"
+  },
+  {
+    "revision": "b19744fe909b8eeeeec9",
+    "url": "/jumplama/static/js/2.0d7b2981.chunk.js"
+  },
+  {
+    "revision": "b9413248ac205c7b72dc",
+    "url": "/jumplama/static/js/main.74ab7dca.chunk.js"
+  },
+  {
+    "revision": "54688bf56de1c94453a1",
+    "url": "/jumplama/static/js/runtime~main.820e7d12.js"
+  },
+  {
+    "revision": "f7a0aaaf3647f77b808c0f4b9623e24f",
+    "url": "/jumplama/static/media/block.f7a0aaaf.png"
+  },
+  {
+    "revision": "6fe51ce68b330a2efd1b2c5168ee9394",
+    "url": "/jumplama/static/media/red-bonus.6fe51ce6.png"
+  }
+]);
